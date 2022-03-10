e427e9d4b721be788866dae2f6c9b1feb8142b9e
haru864 <kawachigenji95@gmail.com>
2022-03-10 22:43:38 +0900

    自機の移動機能を実装

--- src/player/Gun.java
+++ src/player/Gun.java
@@ -2,45 +2,96 @@ package src.player;
 
 import java.awt.Polygon;
 import java.awt.BasicStroke;
-
-import src.Main;
-
 import java.awt.Graphics2D;
 import java.awt.Color;
 
+import src.Main;
+
 public class Gun {
-    private Polygon gun;
+    public Polygon gun;
     public static int HEAD_TO_TAIL = 30; // 自機の機首から尾翼までの長さ
     public static int LEFT_TO_RIGHT = 60; // 自機の主翼の長さ
+    public static int MOVEMENT_UNIT = 5;
 
+    // コンストラクタ
     public Gun() {
         int[] xList = { 30, 60, 50, 40, 30, 20, 10, 0 };
         int[] yList = { 0, 24, 30, 20, 30, 20, 30, 24 };
         gun = new Polygon(xList, yList, xList.length);
     }
 
+    // コンストラクタ
     public Gun(int x, int y) {
         this();
-        for (int i = 0; i < gun.xpoints.length; i++) {
-            gun.xpoints[i] += x;
-            gun.ypoints[i] += y;
+        for (int i = 0; i < this.gun.xpoints.length; i++) {
+            this.gun.xpoints[i] += x;
+            this.gun.ypoints[i] += y;
         }
     }
 
+    // 自機の描画
     public void draw(Graphics2D g2d) {
         g2d.setColor(Color.YELLOW);
-        g2d.fill(gun);
+        g2d.fill(this.gun);
         g2d.setColor(Color.BLACK);
-        g2d.draw(gun);
-        // paintメソッドはコンポーネントを画面に表示したり、
-        // 画面サイズの変更で再描画するタイミングでコールされる。
-        // System.out.println("print");
+        g2d.draw(this.gun);
     }
 
+    // 左に移動
     public void moveLeftGun() {
+        for (int i = 0; i < this.gun.xpoints.length; i++) {
+            this.gun.xpoints[i] -= MOVEMENT_UNIT;
+        }
     }
 
+    // 右に移動
     public void moveRightGun() {
+        for (int i = 0; i < this.gun.xpoints.length; i++) {
+            this.gun.xpoints[i] += MOVEMENT_UNIT;
+        }
+    }
+
+    // 上に移動
+    public void moveUpGun() {
+        for (int i = 0; i < this.gun.xpoints.length; i++) {
+            this.gun.ypoints[i] -= MOVEMENT_UNIT;
+        }
+    }
+
+    // 下に移動
+    public void moveDownGun() {
+        for (int i = 0; i < this.gun.xpoints.length; i++) {
+            this.gun.ypoints[i] += MOVEMENT_UNIT;
+        }
+    }
+
+    // 前後左右の座標を返す
+    public int getEdge(String dir) {
+        int min_x, max_x, min_y, max_y;
+        min_x = Integer.MAX_VALUE;
+        max_x = Integer.MIN_VALUE;
+        min_y = Integer.MAX_VALUE;
+        max_y = Integer.MIN_VALUE;
+
+        for (int x : this.gun.xpoints) {
+            min_x = Math.min(min_x, x);
+            max_x = Math.max(max_x, x);
+        }
+
+        for (int y : this.gun.ypoints) {
+            min_y = Math.min(min_y, y);
+            max_y = Math.max(max_y, y);
+        }
+
+        if (dir == "left")
+            return min_x;
+        if (dir == "right")
+            return max_x;
+        if (dir == "top")
+            return min_y;
+        if (dir == "bottom")
+            return max_y;
+        return -1;
     }
 
     public void setGun() {
--- src/screen/GamePanel.java
+++ src/screen/GamePanel.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
@@ -12,20 +14,54 @@ import javax.swing.text.AttributeSet.ColorAttribute;
 import src.Main;
 import src.player.Gun;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener {
+    public Gun player;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT));
         this.setBackground(Color.black);
+
+        // KeyLietenerを追加
+        this.setFocusable(true); // パネルでキーを受付ける
+        this.addKeyListener(this);
+
+        // 自機オブジェクトを生成
+        player = new Gun((Main.SCREEN_WIDTH - Gun.LEFT_TO_RIGHT) / 2,
+                Main.SCREEN_HEIGHT - Gun.HEAD_TO_TAIL - 20);
     }
 
+    // 背景と自機を描画
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setBackground(Color.black);
         g2.clearRect(0, 0, getWidth(), getHeight());
-        // Gun gun = new Gun(60, Main.SCREEN_HEIGHT - Gun.HEAD_TO_TAIL);
-        Gun gun = new Gun((Main.SCREEN_WIDTH - Gun.LEFT_TO_RIGHT) / 2,
-                Main.SCREEN_HEIGHT - Gun.HEAD_TO_TAIL - 20);
-        gun.draw(g2);
+        player.draw(g2);
+        // paintメソッドはコンポーネントを画面に表示したり、
+        // 画面サイズの変更で再描画するタイミングでコールされる。
+        // System.out.println("paint");
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT
+                && player.getEdge("left") >= Gun.MOVEMENT_UNIT) {
+            player.moveLeftGun();
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT
+                && player.getEdge("right") <= (Main.SCREEN_WIDTH - Gun.MOVEMENT_UNIT)) {
+            player.moveRightGun();
+        } else if (e.getKeyCode() == KeyEvent.VK_UP
+                && player.getEdge("top") >= Gun.MOVEMENT_UNIT) {
+            player.moveUpGun();
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN
+                && player.getEdge("bottom") <= Main.SCREEN_HEIGHT - Gun.MOVEMENT_UNIT) {
+            player.moveDownGun();
+        }
+        repaint(); // call paint() method
+    }
+
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
     }
 
     public void update(Graphics g) {
